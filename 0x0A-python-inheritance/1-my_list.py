334c30da030f834b5234fbe9d0ccee62e2505c07
T2085371P <111936741+T2085371P@users.noreply.github.com>
2023-01-10 02:27:22 +0200

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,24 @@
+
 #!/usr/bin/python3
+
+"""
+Contains class MyList
+inherits from list; has public instance method to print sorted
+"""
+
+
+
+
+
 class MyList(list):
-    """ Class that inherits the attributes references of class list
-    Args:
-        list: class list
+
+    """inherits from list
+    methods:
+    print_sorted(self)
     """
 
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+
+        """prints list of ints all sorted in ascending order"""
+
+        print(sorted(self))
