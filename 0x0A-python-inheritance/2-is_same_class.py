452afd617d27eac4c7d54c243c7b939b5a50e61a
T2085371P <111936741+T2085371P@users.noreply.github.com>
2023-01-10 15:28:19 +0200

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,11 +1,8 @@
+
 #!/usr/bin/python3
+"""checks for type of class against object"""
+
+
 def is_same_class(obj, a_class):
-    """ Function that returns True/False if obj is a type of a_class
-    Args:
-        obj: object
-        a_class: class type
-    Returns:
-        True if type of obj is a_class
-        False, otherwise
-    """
+    """returns"""
     return type(obj) is a_class
