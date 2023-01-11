7a01ac0ca9fce08771ac533bc62b6e236377cab7
T2085371P <111936741+T2085371P@users.noreply.github.com>
2023-01-11 02:59:55 +0200

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a file-appending function."""
+"""Defines a file-writing function."""
 
 
-def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file.
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
     Args:
-        filename (str): The name of the file to append to.
-        text (str): The string to append to the file.
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
     Returns:
-        The number of characters appended.
+        The number of characters written.
     """
-    with open(filename, "a", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
