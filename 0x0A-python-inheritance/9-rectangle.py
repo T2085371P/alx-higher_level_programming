71ee6569043effe40ca212f64d4eadd256e034f2
T2085371P <111936741+T2085371P@users.noreply.github.com>
2023-01-11 02:49:06 +0200

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,3 +6,21 @@ and is based on 8-rectangle.py
 """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rect class """
+    def __init__(self, width, height):
+        """instantiation of the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ Returns the area of a rectangle """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """Str representation of rect """
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
