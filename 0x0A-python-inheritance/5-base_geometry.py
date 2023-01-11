a5627ddf445f17ef0a8dcd572fe4348bd541e869
T2085371P <111936741+T2085371P@users.noreply.github.com>
2023-01-11 02:35:47 +0200

    Update 5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """ Empty class """
+"""
+This module defines an empty BaseGeometry
+class
+"""
+
+
+class BaseGeometry():
+    """An empty class"""
     pass
